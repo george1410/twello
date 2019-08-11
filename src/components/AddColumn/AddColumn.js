b521a9dc9b2e6816f19a8a5755bef24ee25c1b29
George McCarron <hello@georgemccarron.com>
2019-08-11 08:29:30 +0100

    Update styling

--- src/components/AddColumn/AddColumn.js
+++ src/components/AddColumn/AddColumn.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './AddColumn.module.css';
 
 const AddColumn = () => {
-  return <div className={styles.root}>+ Add Another Column</div>;
+  return <div className={styles.root}>Add Another Column</div>;
 };
 
 export default AddColumn;
