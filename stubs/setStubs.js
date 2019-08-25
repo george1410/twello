e581d8e40e14bbe629a26add5635110a2188de52
George McCarron <hello@georgemccarron.com>
2019-08-25 10:20:10 +0100

    Change to using stubby for api stubs

---
+++ stubs/setStubs.js
@@ -0,0 +1,22 @@
+import { Stubby } from 'stubby';
+import boardStubs from './boards/boards.json';
+import columnStubs from './boards/columns.json';
+import cardStubs from './boards/cards.json';
+
+const stubs = new Stubby();
+
+stubs.start({ stubs: 3001 });
+
+const data = [boardStubs, columnStubs, cardStubs];
+
+data.forEach(stub => {
+  stubs.post(stub, (err, endpoint) =>
+    console.log(
+      `${endpoint.request.method} ${endpoint.request.url} added successfully.`
+    )
+  );
+});
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
+process.stdin.on('data', process.exit.bind(process, 0));
